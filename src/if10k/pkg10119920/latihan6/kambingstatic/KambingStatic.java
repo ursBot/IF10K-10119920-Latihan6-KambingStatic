92dab7c67344fa6d92d655f3464414c4ca66ddaf
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:13:31 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan6/kambingstatic/KambingStatic.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan6.kambingstatic;
+
+/**
+ *
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+class Mamalia {
+    // Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
+
+public class KambingStatic{
+    // NAMA_KAMBING sebagai konstatnta
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+
+}
